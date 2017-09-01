74b80b90d4ce0e305f9fc3059c4a57b05fa86005
gtla6224422 <nonop01@163.com>
2017-09-01 20:58:15 +0800

    type()

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ import run_game,click_rune,click_rune_720x1280,click_expup
 
 #d = Global.start(Global.DEVICE,Global.APPID_WETEST)
 #print Global.getHW(d)[0]
+
+def mian():
+	click_expup.click_expup(Global.DEVICE, Global.APPID_WETEST)
+
 if __name__ == "__main__":
-	click_expup.click_expup(Global.DEVICE,Global.APPID_WETEST)
+	mian()
